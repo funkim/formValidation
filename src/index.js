a5e384a8ce293c1dbf7169e447d7d54422e0d554
funkim <mcking172@skiff.com>
2024-04-17 22:18:20 -0400

    now goes through the verification of each well.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 import { style } from "./style.css"
+
+
+
+
+function validateEmail() {
+    const email = document.getElementById("email");
+    email.addEventListener("input", () => {
+        if (email.validity.typeMismatch) {
+            email.setCustomValidity("Should be an Email!");
+        } else {
+            email.setCustomValidity(""); 
+        }
+    });
+}
+
+function validateZip() {
+    const zipCode = document.getElementById("zip");
+    zipCode.addEventListener("input", () => {
+        const pattern = /^[0-9]{5}$/;
+        if (!pattern.test(zipCode.value)) {
+            zipCode.setCustomValidity("Invalid ZIP Code. Should be 5 digits.");
+        } else {
+            zipCode.setCustomValidity("");
+        }
+    });
+}
+
+function validatePassword() {
+    const confirmPassword = document.getElementById("confirmPassword");
+    const password = document.getElementById("password");
+    confirmPassword.addEventListener("input", () => {
+        if (password.value !== confirmPassword.value) {
+            confirmPassword.setCustomValidity("Passwords Do Not Match");
+        } else {
+            confirmPassword.setCustomValidity("");
+        }
+    });
+}
+
+validateEmail();
+validatePassword();
+validateZip();
